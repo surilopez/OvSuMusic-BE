eb71c6004ce7c216cdb614f4ffa4ea46b1fed8e2
Emilio Suri Lopez <surilopez@gmail.com>
2021-02-01 17:24:57 -0500

    Controller for productos was added

---
+++ OvSuMusic.WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OvSuMusic.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        //GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Value1", "Value2" };
+        }
+
+        //GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> GetById(int id)
+        {
+            return "Value"+id;
+        }
+
+        //POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        //PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put([FromBody] string value)
+        {
+           
+        }
+
+        //DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+
+        }
+
+
+    }
+}
--- OvSuMusic.WebApi/Startup.cs
+++ OvSuMusic.WebApi/Startup.cs
@@ -5,17 +5,30 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using OvSuMusic.Data;
 
 namespace OvSuMusic.WebApi
 {
     public class Startup
     {
+        private readonly IConfiguration configuration;
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+
+        public Startup(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddControllers();
+
+            services.AddDbContext<TiendaDbContext>(opt => opt.UseSqlServer(configuration.GetConnectionString("default_connection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,10 +43,7 @@ namespace OvSuMusic.WebApi
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllers();
             });
         }
     }
