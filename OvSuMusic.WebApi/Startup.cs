b10b8cc93bd6bb56e574633ed233eebebe434a0c
Emilio Suri Lopez <surilopez@gmail.com>
2021-02-03 17:53:22 -0500

    Configuration for Jwt was added

--- OvSuMusic.WebApi/Startup.cs
+++ OvSuMusic.WebApi/Startup.cs
@@ -1,8 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -12,6 +14,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 using OvSuMusic.Data;
 using OvSuMusic.Data.Contracts;
 using OvSuMusic.Data.Repositories;
@@ -47,23 +50,73 @@ namespace OvSuMusic.WebApi
 
             services.AddScoped<IPasswordHasher<Usuario>, PasswordHasher<Usuario>>();
 
+
+            //Accedemos a la sección JwtSettings del archivo appsettings.json
+            var jwtSettings = configuration.GetSection("JwtConfig");
+            //Obtenemos la clave secreta guardada en JwtSettings:SecretKey
+            string secretKey = jwtSettings.GetValue<string>("SecretKey");
+            //Obtenemos el tiempo de vida en minutos del Jwt guardada en JwtSettings:MinutesToExpiration
+            int minutes = jwtSettings.GetValue<int>("Expiration");
+            //Obtenemos el valor del emisor del token en JwtSettings:Issuer
+            string issuer = jwtSettings.GetValue<string>("Issuer");
+            //Obtenemos el valor de la audiencia a la que está destinado el Jwt en JwtSettings:Audience
+            string audience = jwtSettings.GetValue<string>("Audience");
+
+            var key = Encoding.ASCII.GetBytes(secretKey);
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = true,
+                    ValidIssuer = issuer,
+                    ValidateAudience = true,
+                    ValidAudience = audience,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.FromMinutes(minutes)
+                };
+            });
+
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            });
+
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddSerilog();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
             app.UseRouting();
-
+            app.UseAuthentication();
+            app.UseCors("CorsPolicy");
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+
+
         }
     }
 }
